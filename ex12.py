1f7adad87b32ae8b8b34333f7d85b20e6bfd0a04
Ming Ruan <mruan89@gmail.com>
2015-08-14 09:54:21 -0400

    Add ex12.py

---
+++ ex12.py
@@ -0,0 +1,8 @@
+# Prompting people
+
+#Why is this different than a print and raw_input
+age = raw_input("How old are you? ")
+height = raw_input("How tall are you? ")
+weight = raw_input("How much do you weight? ")
+
+print "So, you are %r old, %r tall and %r heavy." % (age, height, weight)
